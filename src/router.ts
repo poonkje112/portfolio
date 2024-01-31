62fd66428ada231b7cdc59d761492bb167bc29be
Aaron <contact@poonkje.com>
2024-01-31 23:45:10 +0100

    Added a custom browse event

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "@vaadin/router";
+import { BrowseEvent } from "./events";
 import "@/views/pages/";
 
 const router = new Router(document.querySelector("#outlet"));
@@ -7,3 +8,10 @@ router.setRoutes([
   { path: "/about", component: "about-page" },
   { path: "(.*)", component: "not-found-page" },
 ]);
+
+document.addEventListener("browse", (e: any) => {
+  const { target } = e.detail;
+  if(target === "#") return;
+
+  Router.go(target);
+});
