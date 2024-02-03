d00a0e2b8fe7229738f2cc052375a122c4c88fd1
Aaron <contact@poonkje.com>
2024-02-04 00:09:02 +0100

    Added basic navbar [WIP - Mobile only]

---
+++ src/views/components/Nav-Components/navbar-component.ts
@@ -0,0 +1,85 @@
+import { LitElement, html, css } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+@customElement('navbar-component')
+export class NavbarComponent extends LitElement {
+    static styles = [
+        css`
+            section {
+                width: 100%;
+                height: 3em;
+                top: 0;
+                background-color: var(--background-color);
+            }
+
+            ul {
+                position: fixed;
+                width: 100%;
+                height: 100%;
+                z-index: 100;
+                top: 3em;
+                background-color: var(--background-color);
+            }
+
+            ul {
+                list-style: none;
+                padding: 0;
+                margin: 0;
+                width: 100%; /* Make the list full width */
+            }
+
+            li {
+                width: 100%; /* Make all list items full width */
+            }
+
+            h1 {
+                text-align: left; /* Center the text within h1 */
+            }
+
+            /* Center the theme switcher */
+            theme-switcher {
+                width: 100%;
+                display: flex;
+                padding-top: 1em;
+                justify-content: right;
+            }
+
+            .head {
+                width: 100%;
+                display: flex;
+                justify-content: space-between;
+            }
+
+            .head icon-component {
+                cursor: pointer;
+                -webkit-user-select: none; /* Safari */
+                -ms-user-select: none; /* IE 10 and IE 11 */
+                user-select: none; /* Standard syntax */
+            }
+        `,
+    ];
+
+    onHamburgerMenuClick() {
+        this.icon = this.icon === 'menu' ? 'close' : 'menu';
+    }
+
+    @property({ type: String })
+    icon = 'menu';
+
+    render() {
+        return html`
+            <nav>
+                <section class="head">
+                    <h1>Aaron Knoop</h1>
+                    <icon-component icon="${this.icon}" @click="${this.onHamburgerMenuClick}"></icon-component>
+                </section>
+                <ul style="display: ${this.icon === "close" ? "block" : "none"};">
+                    <li><navbar-button label="Test1" href="Lorem"></navbar-button></li>
+                    <li><navbar-button label="Test2" href="Lorem"></navbar-button></li>
+                    <li><navbar-button label="Test3" href="Lorem"></navbar-button></li>
+                    <li><theme-switcher></theme-switcher></li>
+                </ul>
+            </nav>
+        `;
+    }
+}
