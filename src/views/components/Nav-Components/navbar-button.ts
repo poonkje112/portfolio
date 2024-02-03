689f4253211939397e4ef3125d37273e9ecae1f4
Aaron <contact@poonkje.com>
2024-02-04 00:55:28 +0100

    Modified the navbar-button for desktop

--- src/views/components/Nav-Components/navbar-button.ts
+++ src/views/components/Nav-Components/navbar-button.ts
@@ -16,6 +16,40 @@ export class NavbarButton extends LitElement {
             cursor: pointer;
             border-bottom: 1px solid var(--text-color);
         }
+
+        @media (min-width: 1000px) {
+            button {
+                background-color: var(--navbar-bg-color);
+                color: var(--text-color);
+                border: none;
+                cursor: pointer;
+                min-height: 10%;
+                width: 100%;
+                height: 100%;
+                transition: all 0.2s ease-out;
+                align-items: center;
+                justify-content: center;
+                position: relative;
+                margin: 0 10px 0 0;
+                padding: 0;
+
+                &::before {
+                    content: "";
+                    position: absolute;
+                    bottom: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 3px;
+                    background-color: var(--text-color);
+                    transform: scaleX(0);
+                    transition: transform 0.2s ease-out;
+                }
+            }
+
+            button:hover::before {
+                transform: scaleX(1);
+            }
+        }
         `
     ];
 
@@ -23,7 +57,7 @@ export class NavbarButton extends LitElement {
     href = '#';
 
     @property({ type: String })
-    label = 'Sample';
+    label = '';
 
     handleClick() {
         this.dispatchEvent(new BrowseEvent(this.href));
