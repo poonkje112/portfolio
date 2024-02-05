23d2e0da6c71b6037529ac048f8aee6d3b678af1
poonkje112 <contact@poonkje.com>
2024-02-05 16:43:52 +0100

    Refactor import statements in cat-component.ts

--- src/views/components/cat-component.ts
+++ src/views/components/cat-component.ts
@@ -1,5 +1,5 @@
-import { LitElement, html, css, unsafeCSS } from 'lit';
-import { customElement, property } from 'lit/decorators.js'
+import { LitElement, html, css } from 'lit';
+import { customElement } from 'lit/decorators.js'
 
 @customElement('cat-component')
 export class CatComponent extends LitElement {
