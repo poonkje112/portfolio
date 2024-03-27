d26f8c610c3525f93e463574d9c5bfc5018cf171
Aaron <contact@poonkje.com>
2024-03-27 22:34:14 +0100

    Create project-card

---
+++ src/views/components/project-card.ts
@@ -0,0 +1,56 @@
+import { LitElement, html, css } from 'lit';
+import { customElement, property } from 'lit/decorators.js'
+
+@customElement('project-card')
+export class ProjectCard extends LitElement {
+    static styles = [
+        css`
+        article {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            background-color: var(--navbar-bg-color);
+            aspect-ratio: 16/9;
+
+            cursor: pointer;
+        }
+
+        h2 {
+            font-family: var(--default-font);
+            color: var(--text-color);
+        }
+
+        p {
+            font-family: var(--default-font);
+            color: var(--text-color);
+        }
+
+        img {
+            aspect-ratio: 16/9;
+            width: 100%;
+        }
+        `
+    ];
+
+    @property({ type: String })
+    title = '';
+
+    @property({ type: String })
+    description = '';
+
+    @property({ type: String })
+    coverImage = '';
+
+    @property({ type: String })
+    slug = '';
+
+    render() {
+        return html`
+        <article>
+            <img src="${this.coverImage}" alt="${this.title}" />
+            <h2>${this.title}</h2>
+            <p>${this.description}</p>
+        </article>
+        `;
+    }
+}
