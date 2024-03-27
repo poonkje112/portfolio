02c6abb64757692688cc3b2d85c5cefd99c38f68
Aaron <contact@poonkje.com>
2024-03-27 22:33:58 +0100

    Create Services

---
+++ src/services/ImageService.ts
@@ -0,0 +1,13 @@
+import { BASE_URL } from "@/constants";
+import { Image } from "@/model/Image";
+
+export class ImageService {
+    public async getAllImages(projectSlug: string): Promise<Image[]> {
+        const response = await fetch(`${BASE_URL}/images/project/${projectSlug}`);
+        const images = await response.json();
+
+        return images.map((image: any) => {
+            return new Image(image.id);
+        });
+    }
+}
