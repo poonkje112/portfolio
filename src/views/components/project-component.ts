6401d24686c2ab1b9b40f5702539602874937c66
Aaron <contact@poonkje.com>
2024-03-27 23:36:44 +0100

    Removed unused import

--- src/views/components/project-component.ts
+++ src/views/components/project-component.ts
@@ -1,5 +1,5 @@
 import { LitElement, html, css } from 'lit';
-import { customElement, property } from 'lit/decorators.js'
+import { customElement } from 'lit/decorators.js'
 import { ProjectService } from '@/services/ProjectService';
 import { Project } from '@/model/Project';
 
