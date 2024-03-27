7b229ce75e9ad820ae05f3e34669c454de8651a8
Aaron <contact@poonkje.com>
2024-03-28 00:27:15 +0100

    Export the router

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,11 @@
 import { Router } from "@vaadin/router";
 import "@/views/pages/";
 
-const router = new Router(document.querySelector("#outlet"));
+export const router = new Router(document.querySelector("#outlet"));
 router.setRoutes([
   { path: "/", component: "home-page" },
   { path: "/about", component: "about-me" },
+  { path: "/project/:slug", component: "project-page"},
   { path: "(.*)", component: "not-found-page" },
 ]);
 
