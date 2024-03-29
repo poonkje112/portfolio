bccf14b359888d78de874fbeee555e813ac5d990
poonkje112 <contact@poonkje.com>
2024-03-29 17:24:19 +0100

    Implemented image gallery and some project data

--- src/views/pages/project-page.ts
+++ src/views/pages/project-page.ts
@@ -1,10 +1,8 @@
 import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js'
 import { ProjectService } from '@/services/ProjectService';
-import { ImageService } from '@/services/ImageService';
 import { router } from '@/router';
 import { Project } from '@/model/Project';
-import { Image } from '@/model/Image';
 
 @customElement('project-page')
 export class ProjectPage extends LitElement {
@@ -19,34 +17,48 @@ export class ProjectPage extends LitElement {
             font-family: var(--default-font);
             color: var(--text-color);
         }
+
+        img {
+            width: 100%;
+            height: auto;
+        }
+
+        section {
+            width: 100%;
+            height: calc(100vh - var(--navbar-height));
+            background-color: var(--background-color);
+            display: grid;
+            grid-template-columns: 1fr 2fr;
+            gap: 1em;
+            padding: 1em;
+        }
+
+        #imgContainer {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            gap: 1em;
+            padding: 1em;
+            justify-content: center;
+        }
+
+
         `
     ];
 
     private slug = '';
 
     private projectService = new ProjectService();
-    private imageService = new ImageService();
-
-    private images: Image[] = [];
     private project: Project | undefined;
 
     connectedCallback() {
         super.connectedCallback();
         this.slug = router.location.params.slug as string;
+        window.scrollTo(0, 0);
     }
 
     async firstUpdated() {
         const project = await this.projectService.getProject(this.slug);
-        const allImages = await this.imageService.getAllImages(this.slug);
-
         this.project = project;
-        this.images = allImages;
-
-        // Set the cover image as the first image
-        const coverImage = allImages.find((image: Image) => image.isCover);
-        if(coverImage) {
-            this.images = [coverImage, ...allImages.filter((image: Image) => !image.isCover)];
-        }
         
         this.requestUpdate();
     }
@@ -54,14 +66,14 @@ export class ProjectPage extends LitElement {
 
     render() {
         return html`
-        <section>
-            <div>
-                <h1>${this.project?.title}</h1>
-                <p>${this.project?.description}</p>
-            </div>
-            <div>
-                ${this.images.map((image: Image) => html`<img src="${image.path}" />`)}
-            </div>
-        </section>`;
+        <padded-container>
+            <section>
+                <image-galery projectSlug="${this.slug}"></image-galery>
+                <div>
+                    <h1>${this.project?.title}</h1>
+                    <p>${this.project?.description}</p>
+                </div>
+            </section>
+        </padded-container>`;
     }
 }
